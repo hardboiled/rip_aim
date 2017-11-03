4f92ce1917198eface41475dbf482f8e329fbcca
Daniel Ege <daniel.c.ege@gmail.com>
2017-11-03 09:27:45 -0400

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+return unless ['development', 'qa'].include?(Rails.env)
+
+users = (1..10).to_a.map do |i|
+  User.create!(username: "myusername#{i}", password: 'passw0rd')
+end
+
+users.each do |sender|
+  potential_recipients = users - [sender]
+  50.times do
+    my_index = rand(1..potential_recipients.length) - 1
+    recipient = potential_recipients[my_index]
+    Message.create!(
+      sender: sender, recipient: recipient, content: 'I <3 swimming',
+      message_type: 'text', metadata: '{ "tags": [ "my_fair_lady", "jovial", "that_sunset_tho" ] }'
+    )
+  end
+end
