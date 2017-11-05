e97cff85de526c491eb632440737597726b1052b
Daniel Ege <daniel.c.ege@gmail.com>
2017-11-04 21:47:40 -0400

    update db to have index on username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030025232) do
+ActiveRecord::Schema.define(version: 20171105014514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20171030025232) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username"
   end
 
   add_foreign_key "messages", "users", column: "recipient_id"
